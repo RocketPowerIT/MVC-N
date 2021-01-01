0abf15d55a3124d03389e4dcad3841e76c0542ef
Dmitry Kokurkin <wolfyteze@gmail.com>
2021-01-01 21:42:20 +0300

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by wolfyteze on 01/01/2021.
+//  Copyright © 2021 wolfyteze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
